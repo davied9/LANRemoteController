513597d805c1a15131ef93f3527c7162a92e0675
davied <wuwei_543333827@126.com>
2018-05-20 21:51:42 +0800

    raw example for client and server (ok to run)

--- ClientWin/ClientWin.py
+++ ClientWin/ClientWin.py
@@ -1,19 +1,22 @@
 from socket import *
+import time
 
 
 class LRCClient(object):
     def __init__(self):
         self.is_working = False
         self.serve_addr = ('localhost', 33520)
-        self.round = 0
+        self.round = -1
     # interfaces
     def Start(self):
+        self.round = self.round + 1
 
         s = socket(AF_INET, SOCK_STREAM)
 
-        hello_connection = s.connct(self.serve_addr)
-        s.connected
-
+        hello_connection = s.connect(self.serve_addr)
+        time.sleep(1)
+        s.send(('hello from %d' % self.round).encode('utf-8'))
+        s.close()
 
         pass
     def Stop(self):
@@ -21,7 +24,10 @@ class LRCClient(object):
 
 def test000():
     client = LRCClient()
-    client.Start()
+    for round in range(10):
+        client.round = round - 1
+        client.Start()
+    
     pass
 
 
--- Server/Server.py
+++ Server/Server.py
@@ -8,12 +8,13 @@ class LRCServer ( object ):
         self.serve_addr = ('localhost', 33520)
         self.serve_thread = None
         self.listener = None
-        self.round = 0
+        self.round = -1
     # interfaces
     def Start(self):
-        self.serve_thread = threading.Thread(self.__respond_thread)
-        self.serve_thread.start()
+        self.serve_thread = threading.Thread(target=self.__respond_thread)
         self.is_working = True
+        self.serve_thread.start()
+        print('start serving on', self.serve_addr)
         pass
     def Stop(self):
         self.serve_thread._stop()
@@ -26,12 +27,11 @@ class LRCServer ( object ):
         self.listener.bind(self.serve_addr)
         self.listener.listen(10)
 
-        self.round = -1
         while True:
-            ++self.round
+            self.round = self.round + 1
             hello_connection, client_addr = self.listener.accept()
             print('round ', self.round, 'accepted : ', hello_connection, ' - ', client_addr)
-            print('from hello :', hello_connection.recv(1024))
+            print('from hello :', hello_connection.recv(1024).decode())
             hello_connection.close()
             
 
