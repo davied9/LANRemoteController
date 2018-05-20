fe5fcd54ef58231de3d5470e3d3fcab48a935704
davied <wuwei_543333827@126.com>
2018-05-21 07:48:45 +0800

    update T001

--- Test/T001_UsingKivy.py
+++ Test/T001_UsingKivy.py
@@ -4,15 +4,34 @@ import kivy
 # kivy.require('1.9.1')
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 
 
-class test(App):
+class HelloWorld(App):
     def build(self):
         return Button(text='hello world')
 
+def test000():
+    HelloWorld().run()
+
+
+class MultiButton(Widget):
+    def __init__(self, **kwargs):
+        super(MultiButton, self).__init__(**kwargs)
+        self.one = Button(text='one')
+        self.two = Button(text='two')
+        pass
+
+class test(App):
+    def build(self):
+        return MultiButton()
+
+def test001():
+    test().run()
 
 if __name__ == '__main__':
-    test().run()
+    test001()
+    pass
 
 
 
