30c92d0161cc92b56484e0490357008d74218e77
Wei.Wu <Wei.Wu@alltechmed.com>
2018-05-28 15:27:40 +0800

    remove definition of LRCServerAPP

--- Server/ServerUI.py
+++ Server/ServerUI.py
@@ -3,23 +3,13 @@ from __future__ import print_function
 
 from Common.KivyImporter import *
 
-
-class LRCServerApp(App):
+class LRCServerUI(App):
 
     def build(self):
-        return LRCServerUI()
-
-
-class LRCServerUI(BoxLayout):
-
-    def __init__(self, **kwargs):
-        BoxLayout.__init__(self, **kwargs)
-        self.orientation = 'vertical'
-        self.pos_hint = { 'top' : 1, 'x' : 0 }
-        self.size_hint_min = [800,600]
+        self.main = BoxLayout(orientation='vertical', pos_hint={'top': 1, 'x': 0}, size_hint_min=[800, 600])
         # up : start/stop buttons
         up_grid = GridLayout(cols=6, padding=10, spacing=10, size_hint_max_y=100)
-        self.add_widget(up_grid)
+        self.main.add_widget(up_grid)
         up_grid.add_widget(Button(text="Start Server", size_hint_max_x=140))
         up_grid.add_widget(Label(text="IP", size_hint_max_x=40))
         up_grid.add_widget(TextInput(text='text default', size_hint_max_x=140))
@@ -33,13 +23,12 @@ class LRCServerUI(BoxLayout):
         up_grid.add_widget(TextInput(text='text default'))
         up_grid.add_widget(Label())
         # down : log window
-        #down_box = BoxLayout()
-        self.add_widget(Label(text='Log window', size_hint=[0.9,0.9], pos_hint={'center_x':0.5, 'center_y':0.5}))
-        #down_box.add_widget(Label(text='Log window'))
+        self.main.add_widget(Label(text='Log window', size_hint=[0.9, 0.9], pos_hint={'center_x': 0.5, 'center_y': 0.5}))
+        return self.main
 
 
 def __test001_basics():
-    LRCServerApp().run()
+    LRCServerUI().run()
     pass
 
 
