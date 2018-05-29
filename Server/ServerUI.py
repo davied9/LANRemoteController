1b32164a5d3b88d06d8c140c947c235b9781d8ab
davied <wuwei_543333827@126.com>
2018-05-29 23:20:59 +0800

    add server watcher && waiter watcher

--- Server/ServerUI.py
+++ Server/ServerUI.py
@@ -2,7 +2,9 @@
 from __future__ import print_function
 from Common.KivyImporter import *
 from multiprocessing import Process
+from threading import Thread
 from random import randint
+from time import sleep
 
 def start_LRCServer(server_address, waiter_address):
     from LRCServer import LRCServer
@@ -54,42 +56,76 @@ class LRCServerUI(App):
         self.server_process = None
         self.waiter_address = ('127.0.0.1', 35527)
         self.waiter_process = None
+        self.watch_interval = 0.5
+
+    def start_server(self):
+        self.server_process = Process(target=start_LRCServer, args=(self.server_address, self.waiter_address))
+        print('start server at :', self.server_address)
+        self.server_process.start()
+        self.server_button.text = 'Close Server'
+        self.server_info_label.text = 'code : ' + str(randint(1000, 9999)) + ', running ...'
+        self.server_ip_input.disabled = True
+        self.server_port_input.disabled = True
+        Thread(target=self.server_watcher).start()
+
+    def stop_server(self):
+        print('terminate server ')
+        self.server_process.terminate()
+        self.server_process = None
+        self.server_button.text = 'Start Server'
+        self.server_info_label.text = ''
+        self.server_ip_input.disabled = False
+        self.server_port_input.disabled = False
+
+    def start_waiter(self):
+        self.waiter_process = Process(target=start_LRCWaiter, args=(self.waiter_address, ))
+        print('start waiter at :', self.waiter_address)
+        self.waiter_process.start()
+        self.waiter_button.text = 'Close Waiter'
+        self.waiter_info_label.text = 'running ...'
+        self.waiter_ip_input.disabled = True
+        self.waiter_port_input.disabled = True
+        Thread(target=self.waiter_watcher).start()
+
+    def stop_waiter(self):
+        print('terminate waiter ')
+        self.waiter_process.terminate()
+        self.waiter_process = None
+        self.waiter_button.text = 'Start Waiter'
+        self.waiter_info_label.text = ''
+        self.waiter_ip_input.disabled = False
+        self.waiter_port_input.disabled = False
+
+    def server_watcher(self):
+        while True:
+            if not self.server_process:
+                break
+            if not self.server_process.is_alive():
+                break
+            sleep(self.watch_interval)
+        self.stop_server()
+
+    def waiter_watcher(self):
+        while True:
+            if not self.waiter_process:
+                break
+            if not self.waiter_process.is_alive():
+                break
+            sleep(self.watch_interval)
+        self.stop_waiter()
 
     def on_start_server_pressed(self, inst):
         if self.server_process: # process is running, close it
-            print('terminate server ')
-            self.server_process.terminate()
-            self.server_process = None
-            self.server_button.text = 'Start Server'
-            self.server_info_label.text = ''
-            self.server_ip_input.disabled = False
-            self.server_port_input.disabled = False
+            self.stop_server()
         else: # start new server
-            self.server_process = Process(target=start_LRCServer, args=(self.server_address, self.waiter_address))
-            print('start server at :', self.server_address)
-            self.server_process.start()
-            self.server_button.text = 'Close Server'
-            self.server_info_label.text = 'code : ' + str(randint(1000, 9999)) + ', running ...'
-            self.server_ip_input.disabled = True
-            self.server_port_input.disabled = True
+            self.start_server()
 
     def on_start_waiter_pressed(self, inst):
         if self.waiter_process: # process is running, close it
-            print('terminate waiter ')
-            self.waiter_process.terminate()
-            self.waiter_process = None
-            self.waiter_button.text = 'Start Waiter'
-            self.waiter_info_label.text = ''
-            self.waiter_ip_input.disabled = False
-            self.waiter_port_input.disabled = False
+            self.stop_waiter()
         else: # start new waiter
-            self.waiter_process = Process(target=start_LRCWaiter, args=(self.waiter_address, ))
-            print('start waiter at :', self.waiter_address)
-            self.waiter_process.start()
-            self.waiter_button.text = 'Close Waiter'
-            self.waiter_info_label.text = 'running ...'
-            self.waiter_ip_input.disabled = True
-            self.waiter_port_input.disabled = True
+            self.start_waiter()
+
 
 
 def __test001_basics():
