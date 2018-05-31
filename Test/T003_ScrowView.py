62f60c595ac4f3d6e47dc8fd71db73e4e0a513d2
davied <wuwei_543333827@126.com>
2018-05-31 21:19:10 +0800

    T003_ScrollView : normal scrollview can add button

--- Test/T003_ScrowView.py
+++ Test/T003_ScrowView.py
@@ -1,21 +1,38 @@
 from KivyImporter import *
 
+
+
+
 class Main(App):
 
     def build(self):
-        root = ScrollView()
-        layout = BoxLayout(orientation='vertical', size_hint=(1,None))
-        root.add_widget(layout)
+        root = BoxLayout(orientation='vertical', padding=30, spacing=10)
+        root.add_widget(Button(text='add', size_hint=(0.6,0.1), pos_hint={'center_x':0.5, 'center_y':0.5},
+                               on_press=self.add_button))
 
-        for i in range(50):
+        scroll = ScrollView()
+        root.add_widget(scroll)
+
+        layout = BoxLayout(orientation='vertical', size_hint=(1,None))
+        scroll.add_widget(layout)
+
+        layout.bind(minimum_height=layout.setter('height'))
+
+        self.index = 7
+        for i in range(self.index):
             text = 'joker {0}'.format(i)
             layout.add_widget(Button(text=text, size_hint=(1,None), height=60))
 
-        self.layout=layout
+        self.scroll_layout = layout
         return root
 
     def on_start(self):
-        self.layout.bind(minimum_height=self.layout.setter('height'))
+        pass
+
+    def add_button(self, _):
+        self.index += 1
+        text = 'joker {0}'.format(self.index)
+        self.scroll_layout.add_widget(Button(text=text, size_hint=(1,None), height=60))
 
 if '__main__' == __name__:
     Main().run()
