7368667f712ed66414240923e7d93eaeabce02e7
davied <wuwei_543333827@126.com>
2018-06-14 22:36:07 +0800

    cache build command for p4a

--- build.bat
+++ build.bat
@@ -5,19 +5,22 @@ if "%1%"=="" goto End
 if %1%==server (
 echo build server for win32
 python3 -m PyInstaller --name server .\Server\ServerUI.py
-goto End
 )
 
 if %1%==client (
 echo build client for win32
 python3 -m PyInstaller --name client .\ClientWin\ClientUI.py
-goto End
 )
 
 if %1%==test (
 echo build test for win32
 python3 -m PyInstaller --name test .\Test\T007_kivy_start.py
-goto End
+)
+
+if %1%==temp (
+echo build client for android
+echo this is not ready !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+p4a apk --private .\ClientWin\ClientUI.py --package=dav.LRC.LRCClient --name "LRCClient" --version 0.1 --bootstrap=sdl2 --requirements=python3,kivy
 )
 
 :End
