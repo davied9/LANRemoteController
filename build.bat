f96942f9cfc3f7671361409fda6ac129150d801c
davied <wuwei_543333827@126.com>
2018-06-14 22:23:18 +0800

    add build log

--- build.bat
+++ build.bat
@@ -3,16 +3,19 @@
 if "%1%"=="" goto End
 
 if %1%==server (
+echo build server for win32
 python3 -m PyInstaller --name server .\Server\ServerUI.py
 goto End
 )
 
 if %1%==client (
+echo build client for win32
 python3 -m PyInstaller --name client .\ClientWin\ClientUI.py
 goto End
 )
 
 if %1%==test (
+echo build test for win32
 python3 -m PyInstaller --name test .\Test\T007_kivy_start.py
 goto End
 )
