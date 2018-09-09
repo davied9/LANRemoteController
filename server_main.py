28a21119f8a4e6c745653ca8071f57f963ef668c
davied <wuwei_543333827@126.com>
2018-09-09 10:10:24 +0800

    server_main.py : initialize command line related interface

--- server_main.py
+++ server_main.py
@@ -2,6 +2,12 @@
 if __name__ == '__main__':
     import os, sys; sys.path.append(os.path.dirname(sys.argv[0]))
 
+    def get_default_config():
+        pass
+
+    def parse_arguments():
+        pass
+
     import Common.logger
     from multiprocessing import freeze_support
     from Server.ServerUI import LRCServerUI
