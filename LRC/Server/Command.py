2b49dcd0eaa6afc91f3b1adf02111f4b89d363bb
davied <wuwei_543333827@126.com>
2018-09-25 19:20:51 +0800

    add command test cases

--- LRC/Server/Command.py
+++ LRC/Server/Command.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 
 class BaseCommand(object):
 
-    def execute(self, **kwargs):
+    def execute(self, **_kwargs):
         pass
 
 class Command(BaseCommand):
@@ -16,22 +16,66 @@ class Command(BaseCommand):
             self.kwargs = kwargs
 
     def __str__(self):
-        return  '{} {} {}'.format(self.name, self._execute_handler, self.kwargs)
+        return  '<{} :: {} :: {}>'.format(self.name, self._execute_handler, self.kwargs)
 
     def execute(self, **kwargs):
         kwargs.update(self.kwargs)
         self._execute_handler(**kwargs)
 
 
-def parse_command(**kwargs):
-    if 'import' not in kwargs or 'interface' not in kwargs:
-        raise ValueError('"import" and "interface" should be specified for command')
+def _get_simple_interface(interface, module):
+    if interface.startswith(module):
+        return interface[len(module):]
+    else:
+        return interface
 
-    module = kwargs['import']
-    interface = kwargs['interface']
 
-    exec('import ' + module)
-    return getattr(eval(module), interface)()
+def parse_command(**_kwargs):
+    '''
+    parse one command from settings
+    :param **kwargs:    setting for command
+    :return command:    command parsed from settings
+    available settings :
+        import      -- the module will import
+        kwargs      -- parameters for command execution
+        execute     -- attribute of module, used as execute handler for common command (LRC.Server.Command.Command)
+        command     -- command class
+    '''
+    if 'import' in _kwargs:
+        module = _kwargs['import']
+        exec('import ' + module)
+        del _kwargs['import']
+    else:
+        module = ''
+
+    if 'kwargs' in _kwargs:
+        kwargs = eval(_kwargs['kwargs'])
+        del _kwargs['kwargs']
+    else:
+        kwargs = dict()
+
+    if 'interface' in _kwargs: # interface to get command instance
+        interface = _get_simple_interface(_kwargs['interface'], module)
+        del _kwargs['interface']
+        return getattr(eval(module), interface)(kwargs=kwargs, **_kwargs)
+
+    if 'command' in _kwargs: # command class
+        command_class = _get_simple_interface(_kwargs['command'], module)
+        del _kwargs['command']
+        return getattr(eval(module), command_class)(kwargs=kwargs, **_kwargs)
+
+    if 'execute_interface' in _kwargs: # interface to get execute handler for a common command (LRC.Server.Command.Command)
+        execute_interface = _get_simple_interface(_kwargs['execute_interface'], module)
+        del _kwargs['execute_interface']
+        return Command(name="parsed from string", execute=getattr(eval(module), execute_interface)(), kwargs=kwargs, **_kwargs)
+
+    if 'execute' in _kwargs: # execute handler for a common command (LRC.Server.Command.Command)
+        execute = _get_simple_interface(_kwargs['execute'], module)
+        del _kwargs['execute']
+        return Command(name="parsed from string", execute=getattr(eval(module), execute), kwargs=kwargs, **_kwargs)
+
+    raise ValueError('parse_command : one of the following should be specified : {}'.format(
+            {'interface','command','execute_interface','execute'}))
 
 
 if '__main__' == __name__:
