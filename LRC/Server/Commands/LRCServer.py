a6f47ec0c852b7f3e68008344173015d140bd76e
davied <wuwei_543333827@126.com>
2018-09-27 20:37:41 +0800

    LRCServer.py : add manager instance interface

--- LRC/Server/Commands/LRCServer.py
+++ LRC/Server/Commands/LRCServer.py
@@ -138,19 +138,47 @@ class LRCServerManager(object):
             logger.error('LRC : start waiter process failed : {}'.format(err.args))
 
 
+_manager = None
+def manager():
+    global _manager
+    if not _manager:
+        _manager = LRCServerManager()
+    return _manager
+
+
 if '__main__' == __name__:
-    from LRC.Server.Config import LRCServerConfig
-    from time import sleep
-    freeze_support()
 
-    manager = LRCServerManager()
+    def test_case_000(): # basic usage
+        from LRC.Server.Config import LRCServerConfig
+        from time import sleep
+        freeze_support()
 
-    config = LRCServerConfig()
-    config.server_port = 35530
-    config.waiter_port = 35527
+        manager = LRCServerManager()
 
-    manager.start_server(**config.server_config)
-    manager.start_waiter(**config.waiter_config)
+        config = LRCServerConfig()
+        config.server_port = 35530
+        config.waiter_port = 35527
 
-    sleep(30)
-    manager.quit()
+        manager.start_server(**config.server_config)
+        manager.start_waiter(**config.waiter_config)
+
+        sleep(30)
+        manager.quit()
+
+
+    def test_case_001():
+        from LRC.Server.Config import LRCServerConfig
+        from time import sleep
+
+        config = LRCServerConfig()
+        config.server_port = 35530
+        config.waiter_port = 35527
+
+        manager().start_server(**config.server_config)
+        manager().start_waiter(**config.waiter_config)
+
+        sleep(30)
+        manager().quit()
+
+
+    test_case_001()
