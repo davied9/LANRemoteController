9c9c7bf6dac20ce58dd95fe5dabb2f8d20fc3244
davied <wuwei_543333827@126.com>
2018-09-28 18:37:34 +0800

    LRCServerConfig : add config parse process for server, waiter, ....

--- LRC/Server/Config.py
+++ LRC/Server/Config.py
@@ -2,17 +2,51 @@
 
 class LRCServerConfig(object):
 
-    def __init__(self):
-        self.config_file = None
-        self.enable_ui = False
+    def __init__(self, **kwargs):
+        self.config_file = None if 'config_file' not in kwargs else kwargs['config_file']
+        self.enable_ui = False if 'enable_ui' not in kwargs else kwargs['enable_ui']
+
         self.command_ip = '127.0.0.1'
         self.command_port = 32781
+        self._update_command_server_config(**kwargs)
+
         self.server_ip = '0.0.0.0'
         self.server_port = 35530
+        self._update_server_config(**kwargs)
+
         self.waiter_ip = '0.0.0.0'
         self.waiter_port = 35527
+        self._update_waiter_config(**kwargs)
+        
         self.verify_code = None # for new client verification
         self.verbose = False
+
+    def _update_command_server_config(self, **kwargs):
+        if 'command_server_address' in kwargs:
+            self.command_ip = kwargs['command_server_address'][0]
+            self.command_port = kwargs['command_server_address'][1]
+        if 'command_server_ip' in kwargs:
+            self.command_ip = kwargs['command_server_ip']
+        if 'command_server_port' in kwargs:
+            self.command_port = kwargs['command_server_port']
+
+    def _update_server_config(self, **kwargs):
+        if 'server_address' in kwargs:
+            self.server_ip = kwargs['server_address'][0]
+            self.server_port = kwargs['server_address'][1]
+        if 'server_ip' in kwargs:
+            self.server_ip = kwargs['server_ip']
+        if 'server_port' in kwargs:
+            self.server_port = kwargs['server_port']
+
+    def _update_waiter_config(self, **kwargs):
+        if 'waiter_address' in kwargs:
+            self.waiter_ip = kwargs['waiter_address'][0]
+            self.waiter_port = kwargs['waiter_address'][1]
+        if 'waiter_ip' in kwargs:
+            self.waiter_ip = kwargs['waiter_ip']
+        if 'waiter_port' in kwargs:
+            self.waiter_port = kwargs['waiter_port']
 
     @property
     def command_server_address(self):
@@ -42,8 +76,8 @@ class LRCServerConfig(object):
     @property
     def waiter_config(self):
         return {
-            'connect_server_address'  : self.server_address,
-            'waiter_address'           : self.waiter_address,
+            'server_address'  : self.server_address,
+            'waiter_address'  : self.waiter_address,
         }
 
     def __str__(self):
