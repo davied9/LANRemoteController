daf121c12d9e2cdbf2dda8ee92cf0b2a279b17ed
Wei.Wu <Wei.Wu@alltechmed.com>
2019-07-31 13:20:16 +0800

    add new entry for server

--- LRC/server_main.py
+++ LRC/server_main.py
@@ -1,8 +1,27 @@
 from __future__ import print_function
 from LRC.Common.logger import logger
+from LRC.Common.info import url
+
+__description__ = '''
+LANRemoteController
+
+[example]
+    lrcserver --no-ui start_lrc --lrc-server-address ('0.0.0.0',35589)
+    lrcserver --no-ui stop_lrc  # you may need to run this in another command window
+
+[more]
+    for more information, see {}
+'''.format(url)
+
+__program__ = 'lrcserver'
 
 
 def main():
+    new_entry()
+    # old_entry()
+
+
+def old_entry():
     import sys
     # parse config
     argv_bac = sys.argv
@@ -142,7 +161,7 @@ def parse_config_from_console_line(args):
     return config, commands, commands_kwargs, reserved
 
 
-def _register_lrc_commands(command_server, config, commands_kwargs):
+def _register_lrc_commands(command_server, config, commands_kwargs=dict()):
     from LRC.Server.Commands.LRCServer import start_lrc, start_lrc_server, start_lrc_waiter
     from LRC.Server.Commands.LRCServer import stop_lrc, stop_lrc_server, stop_lrc_waiter
     from LRC.Server.Commands.LRCServer import quit as quit_lrc
@@ -220,6 +239,98 @@ LRC server
 [more]
     for more infomation, see https://github.com/davied9/LANRemoteController
     '''
+
+
+def new_entry():
+    import sys
+    # parse config
+    config = parse_config_from_console_line_with_parser(sys.argv[1:])
+    # clean arguments
+    sys.argv = [sys.argv[0]]
+    # start server
+    start_lrc_server_new_entry(config)
+
+
+def start_lrc_server_new_entry(config):
+
+    if config.enable_ui:
+        from multiprocessing import freeze_support
+        from LRC.Server.ServerUI import LRCServerUI
+
+        freeze_support()
+        logger.set_logger('kivy')
+
+        if config.commands:
+            logger.warning('LRC : UI is enabled, commands {} will not be executed.'.format(config.commands))
+
+        ui = LRCServerUI()
+        ui.run()
+    else:
+        from LRC.Server.CommandServer import CommandServer
+        import sys
+
+        # start a new command server if necessary
+        command_server = CommandServer()
+        register_lrc_commands(command_server, config)
+        if not command_server.is_running:
+            command_server.start()
+
+        # send the command
+        for cmd in config.commands:
+            command_server.send_command(cmd)
+
+
+def parse_config_from_console_line_with_parser(args):
+    from LRC.Common.info import version, url
+    import argparse
+    parser = argparse.ArgumentParser(description=__description__, prog=__program__)
+    parser.add_argument('--version', '-v', help='version', action='version', version='''
+LRC(LAN Remote Controller) version {}
+{}
+'''.format(version, url))
+    parser.add_argument('--no-ui', help='disable ui', dest='enable_ui', default=True, action='store_false')
+    parser.add_argument('--verbose', '-vv', help='increase log verbose level', default=False, action='store_true')
+    parser.add_argument('--sync-config', help='command server sync local config to main command server',
+                        default=False, action='store_true')
+    parser.add_argument('--config-file', help='configuration file')
+    parser.add_argument('--lrc-server-address', help='lrc server address')
+    parser.add_argument('--lrc-waiter-address', help='lrc waiter address')
+    parser.add_argument('--lrc-verify-code', help='lrc verify code')
+    parser.add_argument('commands', help='commands to execute on command server', nargs='*')
+    return parser.parse_args(args=args)
+
+
+def register_lrc_commands(command_server, config):
+    from LRC.Server.Commands.LRCServer import start_lrc, start_lrc_server, start_lrc_waiter
+    from LRC.Server.Commands.LRCServer import stop_lrc, stop_lrc_server, stop_lrc_waiter
+    from LRC.Server.Commands.LRCServer import quit as quit_lrc
+    from LRC.Server.Command import Command
+
+    # update config from command line
+    lrc_config = dict()
+    if config.lrc_server_address:
+        lrc_config['server_address'] = eval(config.lrc_server_address)
+    if config.lrc_waiter_address:
+        lrc_config['waiter_address'] = eval(config.lrc_waiter_address)
+    if config.lrc_verify_code:
+        lrc_config['verify_code'] = config.lrc_verify_code
+
+    # start/stop LRC
+    command_server.register_command('start_lrc', Command(name='start_lrc', execute=start_lrc, kwargs=lrc_config))
+    command_server.register_command('stop_lrc', Command(name='stop_lrc', execute=stop_lrc))
+
+    # start/stop LRC server
+    command_server.register_command('start_lrc_server', Command(name='start_lrc_server', execute=start_lrc_server, kwargs=lrc_config))
+    command_server.register_command('stop_lrc_server', Command(name='stop_lrc_server', execute=stop_lrc_server))
+
+    # start/stop LRC waiter
+    command_server.register_command('start_lrc_waiter', Command(name='start_lrc_waiter', execute=start_lrc_waiter, kwargs=lrc_config))
+    command_server.register_command('stop_lrc_waiter', Command(name='stop_lrc_waiter', execute=stop_lrc_waiter))
+
+    # quit LRC
+    command_server.register_command('quit_lrc', Command(name='quit_lrc', execute=quit_lrc))
+    command_server.register_cleanup_command('quit_lrc')
+
 
 if __name__ == '__main__':
     main()
